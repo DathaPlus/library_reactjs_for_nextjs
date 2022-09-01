b39a88dc6fc3f5f31cbd69fcb5370c87d337b79d
danidaniel6462 <daniel@dathaplus.com>
2022-09-01 00:57:42 -0500

    add plugin import css

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,8 @@ import replace from "@rollup/plugin-replace";
 import { terser } from "rollup-plugin-terser";
 import { getFolders } from "./scripts/buildUtils";
 import generatePackageJson from "rollup-plugin-generate-package-json";
+import css from "rollup-plugin-import-css";
+
 const packageJson = require("./package.json");
 
 const plugins = [
@@ -20,6 +22,7 @@ const plugins = [
     useTsconfigDeclarationDir: true,
   }),
   terser(),
+  css(),
 ];
 const subfolderPlugins = (folderName) => [
   ...plugins,
